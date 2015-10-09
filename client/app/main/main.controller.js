167fb8c576b3079446ec0722c5ed46695eafc387
yurykorzun <paranoekk@gmail.com>
2015-10-09 15:47:25 -0400

    Added mapbox map to the default html page and neighborhoods retrieval from the database

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,19 +1,43 @@
 angular.module('notinphillyServerApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', [ '$scope', '$http', 'leafletData', function($scope, $http, leafletData) {
     angular.extend($scope, {
                 center: {
                     lat: 39.952604,
                     lng: -75.163368,
                     zoom: 13
                 },
-                layers: {
-                    baselayers: {
-                        xyz: {
-                            name: 'OpenStreetMap (XYZ)',
-                            url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                            type: 'xyz'
-                        }
+                tiles: {
+                   name: 'Not in philly map',
+                   url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
+                   type: 'xyz',
+                   options: {
+                       apikey: 'pk.eyJ1IjoieXVyeWtvcnp1biIsImEiOiJjaWY2eTN2aHMwc3VncnptM3QxMzU3d3hxIn0.Mt0JldEMvvTdWW4GW2RSlQ',
+                       mapid: 'yurykorzun.nljndeg0'
+                   }
+               },
+               controls: {
+                    fullscreen: {
+                        position: 'topleft'
                     }
                 }
             });
-  });
+
+    $http.get("api/neighborhoods").success(function(data, status) {
+        leafletData.getMap().then(function(map) {
+          for(var nIndex = 0; nIndex < data.length; nIndex++)
+          {
+            var geoJsonLayer = L.geoJson(data[nIndex].geodata,
+            {
+              style: {
+                color: '#486CFA',
+                weight: 2,
+                fillOpacity: 0.4,
+                 fillColor: '#78D2FF'
+              }
+            });
+
+            geoJsonLayer.addTo(map);
+          }
+     });
+   });
+  }]);
