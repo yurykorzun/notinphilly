b05732ba91f6fee57f54e9ef7bac302504a7b184
Alex Lubneuski <alex.lubneuski@gmail.com>
2016-03-01 17:25:30 -0500

    syncing branch

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -86,7 +86,6 @@ angular.module('notinphillyServerApp')
 
         newScope.checkin = function() {
           $http.get("api/userstats/checkin/" + $rootScope.currentUser._id).then(function(response){
-            console.log("Success");
             targetPopup._close();
           },
           function(err){
