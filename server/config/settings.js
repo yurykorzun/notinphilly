8def24be04d3cf24b41224e41c3ee81d10c1133e
Alex Lubneuski <alex.lubneuski@gmail.com>
2016-06-16 23:14:23 -0400

    moved the env.process before 127.0.0.1

--- server/config/settings.js
+++ server/config/settings.js
@@ -2,7 +2,7 @@ module.exports.serverSettings = {
   EMAIL_TRASPORT_URL: (process.env.OS_EMAIL_ADDR && process.env.OS_EMAIL_PASSWD) ?
                                       "smtps://" + process.env.OS_EMAIL_ADDR +':'+ process.env.OS_EMAIL_PASSWD + '@host244.hostmonster.com' :
                                       "smtps://notinphilly%40antsdesigns.net:Test123!@host244.hostmonster.com",
-  HTTP_IP: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1" || process.env.AWS_IP,
+  HTTP_IP: process.env.OPENSHIFT_NODEJS_IP || process.env.AWS_IP || "127.0.0.1" ,
   HTTP_PORT: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   DB_CONNECTION_STRING: process.env.OPENSHIFT_MONGODB_DB_URL || process.env.AWS_MONGO_DB_URL || "mongodb://localhost/notinphilly"
 };
