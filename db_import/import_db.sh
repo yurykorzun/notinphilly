a25e8c4c9f5287d61bf7f0e2f0ab03cad0a75db7
Philippe LeSaux <lesauxp@gmail.com>
2016-12-17 12:31:37 -0500

    use project directory for db import

--- db_import/import_db.sh
+++ db_import/import_db.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 echo "Started importing notinphilly db..."
- 
+
 host=localhost
 port=27017
 database="notinphilly"
 
 
-mongoimport -h $host --port $port -d $database -c neighborhoods --authenticationDatabase admin --file neighborhoods_export.json
+mongoimport -h $host --port $port -d $database -c neighborhoods --authenticationDatabase admin --file db_import/neighborhoods_export.json
 
-mongoimport -h $host --port $port -d $database -c roles --authenticationDatabase admin --file roles_export.json
+mongoimport -h $host --port $port -d $database -c roles --authenticationDatabase admin --file db_import/roles_export.json
 
-mongoimport -h $host --port $port -d $database -c sessions --authenticationDatabase admin --file sessions_export.json
+mongoimport -h $host --port $port -d $database -c sessions --authenticationDatabase admin --file db_import/sessions_export.json
 
-mongoimport -h $host --port $port -d $database -c states --authenticationDatabase admin --file states_export.json
+mongoimport -h $host --port $port -d $database -c states --authenticationDatabase admin --file db_import/states_export.json
 
-mongoimport -h $host --port $port -d $database -c streetNames --authenticationDatabase admin --file streetNames_export.json
+mongoimport -h $host --port $port -d $database -c streetNames --authenticationDatabase admin --file db_import/streetNames_export.json
 
-mongoimport -h $host --port $port -d $database -c streetSegments --authenticationDatabase admin --file streetSegments_export.json
+mongoimport -h $host --port $port -d $database -c streetSegments --authenticationDatabase admin --file db_import/streetSegments_export.json
 
-mongoimport -h $host --port $port -d $database -c userProfiles --authenticationDatabase admin --file userProfiles_export.json
+mongoimport -h $host --port $port -d $database -c userProfiles --authenticationDatabase admin --file db_import/userProfiles_export.json
 
-mongoimport -h $host --port $port -d $database -c userStats --authenticationDatabase admin --file userStats_export.json
+mongoimport -h $host --port $port -d $database -c userStats --authenticationDatabase admin --file db_import/userStats_export.json
 
-mongoimport -h $host --port $port -d $database -c zipCodes --authenticationDatabase admin --file zipCodes_export.json
+mongoimport -h $host --port $port -d $database -c zipCodes --authenticationDatabase admin --file db_import/zipCodes_export.json
 
 mongo 'notinphilly' --eval 'printjson(db.getCollection("streetSegments").createIndex( { "geodata.geometry" : "2dsphere" } ))'
 
-echo "Finished importing db..." 
+echo "Finished importing db..."
