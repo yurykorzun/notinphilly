8f6feddcfb31f99cb2cb91a6088a2bcabb173282
Yury Korzun <yury@flyclops.com>
2017-03-12 21:40:06 -0400

    Updated settings

--- server/config/serverSettings.js
+++ server/config/serverSettings.js
@@ -10,5 +10,5 @@ module.exports = {
   SECRET_TOKEN: 'notinphillynotinphilly',
   HTTP_IP: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
   HTTP_PORT: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  DB_CONNECTION_STRING: process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME || process.env.AWS_MONGO_DB_URL || "mongodb://localhost/" + databaseName
+  DB_CONNECTION_STRING: process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME || process.env.MONGODB_URL + databaseName || process.env.AWS_MONGO_DB_URL || "mongodb://localhost/" + databaseName
 };
