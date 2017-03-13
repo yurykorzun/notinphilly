bab32499eae9c5a3748f550d3b3b4d885dcbc692
Yury Korzun <yury@flyclops.com>
2017-03-13 11:20:30 -0400

    Configuration

--- server/config/apiSettings.js
+++ server/config/apiSettings.js
@@ -6,8 +6,8 @@ module.exports = {
   MAP_BOX_API_KEY: "pk.eyJ1IjoieXVyeWtvcnp1biIsImEiOiJjaXVsbHBnOTgwMDc4MnlwazkxYWhoNnY2In0.iU-SAfTXEZSsTEJK74wfeA",
   MAP_BOX_MAP_ID: "yurykorzun.nljndeg0",
   GOOGLE_API_KEY: "AIzaSyAUptjRjLzbBPVOUu2PxQhOLQ5DSNAPEX8",
-  FACEBOOK_APP_ID: "267756506974707",
-  FACEBOOK_SECRET: "bc1675d566b520c5af48f8a09b9e418c",
+  FACEBOOK_APP_ID: "267668170316874",
+  FACEBOOK_SECRET: "e6f762e0d6673e8f6c3539a69819a71d",
   FACEBOOK_NAMESPACE: "notinphilly",
-  FACEBOOK_CALLBACK_URL: serverSettings.WEB_SITE_URL + ":" + serverSettings.HTTP_PORT + "/api/facebook/callback"
+  FACEBOOK_CALLBACK_URL: serverSettings.WEB_SITE_URL + "/api/facebook/callback"
 };
