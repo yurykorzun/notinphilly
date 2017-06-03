50c73d56557df4b7d483cbf126ad835099fba0de
Yury Korzun <yury@flyclops.com>
2017-06-03 13:47:53 -0400

    Updated db import files

--- db_import/export_db_local.bat
+++ db_import/export_db_local.bat
@@ -1,22 +1,21 @@
 echo "Started exporting notinphilly db..."
 
 set host="localhost"
-set database="notinbaltimore"
+set database="notinphilly"
 set port="27017"
+set folder=philadelphia
 
-echo %host% %port% %database% %user% %password%
+echo %host% %port%
 
-call mongoexport -h %host% --port %port% -d %database% -c blocks -o .\baltimore\blocks_export.json
-call mongoexport -h %host% --port %port% -d %database% -c city -o .\baltimore\city_export.json
-call mongoexport -h %host% --port %port% -d %database% -c identitycounters -o .\baltimore\identitycounters_export.json
-call mongoexport -h %host% --port %port% -d %database% -c neighborhoods -o .\baltimore\neighborhoods_export.json
-call mongoexport -h %host% --port %port% -d %database% -c roles -o .\baltimore\roles_export.json
-call mongoexport -h %host% --port %port% -d %database% -c states -o .\baltimore\states_export.json
-call mongoexport -h %host% --port %port% -d %database% -c streets -o .\baltimore\streets_export.json
-call mongoexport -h %host% --port %port% -d %database% -c toolsInventory -o .\baltimore\toolsInventory_export.json
-call mongoexport -h %host% --port %port% -d %database% -c userProfiles -o .\baltimore\userProfiles_export.json
-call mongoexport -h %host% --port %port% -d %database% -c zipcodes -o .\baltimore\zipcodes_export.json
+call mongoexport -h %host% --port %port% -d %database% -c blocks -o .\%folder%\blocks_export.json
+call mongoexport -h %host% --port %port% -d %database% -c city -o .\%folder%\city_export.json
+call mongoexport -h %host% --port %port% -d %database% -c neighborhoods -o .\%folder%\neighborhoods_export.json
+call mongoexport -h %host% --port %port% -d %database% -c roles -o .\%folder%\roles_export.json
+call mongoexport -h %host% --port %port% -d %database% -c states -o .\%folder%\states_export.json
+call mongoexport -h %host% --port %port% -d %database% -c streets -o .\%folder%\streets_export.json
+call mongoexport -h %host% --port %port% -d %database% -c userProfiles -o .\%folder%\userProfiles_export.json
+call mongoexport -h %host% --port %port% -d %database% -c zipcodes -o .\%folder%\zipcodes_export.json
 
-echo "Finished importing db..." 
+echo "Finished exporting db..." 
 
 
