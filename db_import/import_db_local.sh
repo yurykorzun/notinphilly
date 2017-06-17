3ace691f852e377f6382f513f29dd882f205963f
Yury Korzun <yury@flyclops.com>
2017-06-17 10:52:39 -0400

    Fixed the import script

--- db_import/import_db_local.sh
+++ db_import/import_db_local.sh
@@ -8,26 +8,26 @@ folder=philadelphia
 
 echo $host $port $database $folder
 
-mongoimport -h $host --port $port -d $database -c blocks --file ./$folder/blocks_export.js
+mongoimport -h $host --port $port -d $database -c blocks --file ./$folder/blocks_export.js && \
 
-mongoimport -h $host --port $port -d $database -c city --file ./$folder/city_export.js
+mongoimport -h $host --port $port -d $database -c city --file ./$folder/city_export.js && \
 
-mongoimport -h $host --port $port -d $database -c neighborhoods --file ./$folder/neighborhoods_export.json
+mongoimport -h $host --port $port -d $database -c neighborhoods --file ./$folder/neighborhoods_export.json && \
 
-mongoimport -h $host --port $port -d $database -c roles --file ./$folder/roles_export.json
+mongoimport -h $host --port $port -d $database -c roles --file ./$folder/roles_export.json && \
 
-mongoimport -h $host --port $port -d $database -c states --file ./$folder/states_export.json
+mongoimport -h $host --port $port -d $database -c states --file ./$folder/states_export.json && \
 
-mongoimport -h $host --port $port -d $database -c streets --file ./$folder/streets_export.json
+mongoimport -h $host --port $port -d $database -c streets --file ./$folder/streets_export.json && \
 
-mongoimport -h $host --port $port -d $database -c userProfiles--file ./$folder/userProfiles_export.json
+mongoimport -h $host --port $port -d $database -c userProfiles--file ./$folder/userProfiles_export.json && \
 
-mongoimport -h $host --port $port -d $database -c zipCodes --file ./$folder/zipcodes_export.json
+mongoimport -h $host --port $port -d $database -c zipCodes --file ./$folder/zipcodes_export.json && \
 
-call mongo $database --port $port --eval "db.getCollection('neighborhoods').createIndex( { 'geometry' : '2dsphere' })"
+mongo $database --port $port --eval "db.getCollection('neighborhoods').createIndex( { 'geometry' : '2dsphere' })" && \
 
-call mongo $database --port $port --eval "db.getCollection('streets').createIndex( { 'geometry' : '2dsphere' })"
+mongo $database --port $port --eval "db.getCollection('streets').createIndex( { 'geometry' : '2dsphere' })" && \
 
-call mongo $database --port $port --eval "db.getCollection('zipcodes').createIndex( { 'geometry' : '2dsphere' })"
+mongo $database --port $port --eval "db.getCollection('zipcodes').createIndex( { 'geometry' : '2dsphere' })" 
 
 echo "Finished importing db..." 
