a392cd38bdddd660f5cfe8280bf53707aa7b2afa
Yury Korzun <yury@flyclops.com>
2017-07-27 19:52:06 -0400

    Updated the server placeholder settings

--- server/config/serverSettings.js
+++ server/config/serverSettings.js
@@ -4,7 +4,7 @@ module.exports = {
   DATABASE_NAME: process.env.DATABASE_NAME || 'mailgun api key here',
   AUTH_COOKIE_NAME: process.env.AUTH_COOKIE_NAME || 'mailgun api key here',
   SECRET_TOKEN: process.env.SECRET_TOKEN || 'mailgun api key here',
-  HTTP_IP: process.env.EMAIL_API_KEY || 'mailgun api key here',
+  HTTP_IP: process.env.HTTP_IP || 'mailgun api key here',
   HTTP_PORT: process.env.HTTP_PORT || 'mailgun api key here',
   DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING || 'mailgun api key here',
 };
