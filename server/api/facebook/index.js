dd5a3e7e3da163f6ccc489ac327ee46de67daeb3
Yury Korzun <yury@flyclops.com>
2017-10-07 10:07:52 -0400

    test

--- server/api/facebook/index.js
+++ server/api/facebook/index.js
@@ -1,9 +1,26 @@
 var express     = require('express');
 var passport    = require('passport');
+var apiSettings    = require('../../config/apiSettings');
 
 var router = express.Router();
 
-router.get('/', passport.authenticate('facebook', {scope: [ 'public_profile', 'email', 'user_friends']}));
-router.get('/callback', passport.authenticate('facebook',  { failureRedirect: '/public/pages/facebook-error.html' }));
+router.get('/', function(req, res, next) { 
+	console.log(JSON.stringify(apiSettings));
+	console.log(req.originalUrl);
+	console.log(req.hostname);
+	console.log(req.protocol);
+	console.log(req.method);
+	
+	next();
+}, passport.authenticate('facebook', {scope: [ 'public_profile', 'email', 'user_friends']}));
+router.get('/callback',  function(req, res, next) { 
+	console.log(JSON.stringify(apiSettings));
+	console.log(req.originalUrl);
+	console.log(req.hostname);
+	console.log(req.protocol);
+	console.log(req.method);
+	
+	next();
+}, passport.authenticate('facebook',  { failureRedirect: '/public/pages/facebook-error.html' }));
 
 module.exports = router;
