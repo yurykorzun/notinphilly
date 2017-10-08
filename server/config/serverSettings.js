fefb135e4df709fe2ad92f3111f331df72011a0d
Yury Korzun <yury@flyclops.com>
2017-10-08 12:58:46 -0400

    Added string trimming to all config values

--- server/config/serverSettings.js
+++ server/config/serverSettings.js
@@ -1,12 +1,12 @@
 var databaseName = "notinphilly";
 
 module.exports = {
-  ADMIN_EMAIL: process.env.ADMIN_EMAIL || 'mailgun api key here',
-  WEB_SITE_URL: process.env.WEB_SITE_URL || 'mailgun api key here',
-  DATABASE_NAME: process.env.DATABASE_NAME || 'mailgun api key here',
-  AUTH_COOKIE_NAME: process.env.AUTH_COOKIE_NAME || 'mailgun api key here',
-  SECRET_TOKEN: process.env.SECRET_TOKEN || 'mailgun api key here',
-  HTTP_IP: process.env.HTTP_IP || 'mailgun api key here',
-  HTTP_PORT: process.env.HTTP_PORT || 'mailgun api key here',
-  DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING || 'mailgun api key here',
+  ADMIN_EMAIL: process.env.ADMIN_EMAIL.trim() || 'mailgun api key here',
+  WEB_SITE_URL: process.env.WEB_SITE_URL.trim() || 'mailgun api key here',
+  DATABASE_NAME: process.env.DATABASE_NAME.trim() || 'mailgun api key here',
+  AUTH_COOKIE_NAME: process.env.AUTH_COOKIE_NAME.trim() || 'mailgun api key here',
+  SECRET_TOKEN: process.env.SECRET_TOKEN.trim() || 'mailgun api key here',
+  HTTP_IP: process.env.HTTP_IP.trim() || 'mailgun api key here',
+  HTTP_PORT: process.env.HTTP_PORT.trim() || 'mailgun api key here',
+  DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING.trim() || 'mailgun api key here',
 };
