0d49aa478fbad6ae8a12262808c6772e9e2f8082
Yury Korzun <yury@flyclops.com>
2017-10-08 13:12:11 -0400

    Changes to settings config

--- server/config/apiSettings.js
+++ server/config/apiSettings.js
@@ -1,16 +1,16 @@
 
 
 module.exports = {
-  EMAIL_API_KEY: process.env.EMAIL_API_KEY.trim() || 'mailgun api key here',
-  EMAIL_DOMAIN: process.env.EMAIL_DOMAIN.trim() || 'mailgun api key here',
-  MAP_BOX_USER: process.env.MAP_BOX_USER.trim() || 'mailgun api key here',
-  MAP_BOX_API_KEY:process.env.MAP_BOX_API_KEY.trim() || 'mailgun api key here',
-  MAP_BOX_MAP_ID: process.env.MAP_BOX_MAP_ID.trim() || 'mailgun api key here',
-  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY.trim() || 'mailgun api key here',
-  GOOGLE_CALENDAR_ID: process.env.GOOGLE_CALENDAR_ID.trim() || 'mailgun api key here', 
-  FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID.trim() || 'mailgun api key here',
-  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET.trim() || 'mailgun api key here',
-  FACEBOOK_NAMESPACE: process.env.FACEBOOK_NAMESPACE.trim() || 'mailgun api key here',
-  FACEBOOK_CALLBACK_URL: process.env.FACEBOOK_CALLBACK_URL.trim() || 'mailgun api key here',
-  FACEBOOK_GROUP: process.env.FACEBOOK_GROUP.trim() || 'mailgun api key here' 
+  EMAIL_API_KEY: process.env.EMAIL_API_KEY ? process.env.EMAIL_API_KEY.trim() : '' || 'mailgun api key here',
+  EMAIL_DOMAIN: process.env.EMAIL_DOMAIN ? process.env.EMAIL_DOMAIN.trim() : '' || 'mailgun api key here',
+  MAP_BOX_USER: process.env.MAP_BOX_USER ? process.env.MAP_BOX_USER.trim() : '' || 'mailgun api key here',
+  MAP_BOX_API_KEY: process.env.MAP_BOX_API_KEY? process.env.MAP_BOX_API_KEY.trim() : '' || 'mailgun api key here',
+  MAP_BOX_MAP_ID: process.env.MAP_BOX_MAP_ID ? process.env.MAP_BOX_MAP_ID.trim() : '' || 'mailgun api key here',
+  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY ? process.env.GOOGLE_API_KEY.trim() : '' || 'mailgun api key here',
+  GOOGLE_CALENDAR_ID: process.env.GOOGLE_CALENDAR_ID ? process.env.GOOGLE_CALENDAR_ID.trim() : '' || 'mailgun api key here', 
+  FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID ? process.env.FACEBOOK_APP_ID.trim() : '' || 'mailgun api key here',
+  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET ? process.env.FACEBOOK_SECRET.trim() : '' || 'mailgun api key here',
+  FACEBOOK_NAMESPACE: process.env.FACEBOOK_NAMESPACE ? process.env.FACEBOOK_NAMESPACE.trim() : '' || 'mailgun api key here',
+  FACEBOOK_CALLBACK_URL: process.env.FACEBOOK_CALLBACK_URL ? process.env.FACEBOOK_CALLBACK_URL.trim() : '' || 'mailgun api key here',
+  FACEBOOK_GROUP: process.env.FACEBOOK_GROUP ? process.env.FACEBOOK_GROUP.trim() : '' || 'mailgun api key here' 
 };
