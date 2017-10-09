39f94658f1b6fe47ed8d957f7ec1897de91aa09c
Yury Korzun <yury@flyclops.com>
2017-10-09 11:05:53 -0400

    Added config checks

--- server/config/apiSettings.js
+++ server/config/apiSettings.js
@@ -1,16 +1,27 @@
-
+if (!process.env.EMAIL_API_KEY) console.log('config EMAIL_API_KEY missing');
+if (!process.env.EMAIL_DOMAIN) console.log('config EMAIL_DOMAIN missing');
+if (!process.env.MAP_BOX_USER) console.log('config MAP_BOX_USER missing');
+if (!process.env.MAP_BOX_API_KEY) console.log('config MAP_BOX_API_KEY missing');
+if (!process.env.MAP_BOX_MAP_ID) console.log('config MAP_BOX_MAP_ID missing');
+if (!process.env.GOOGLE_API_KEY) console.log('config GOOGLE_API_KEY missing');
+if (!process.env.GOOGLE_CALENDAR_ID) console.log('config GOOGLE_CALENDAR_ID missing');
+if (!process.env.FACEBOOK_APP_ID) console.log('config FACEBOOK_APP_ID missing');
+if (!process.env.FACEBOOK_SECRET) console.log('config FACEBOOK_SECRET missing');
+if (!process.env.FACEBOOK_NAMESPACE) console.log('config FACEBOOK_NAMESPACE missing');
+if (!process.env.FACEBOOK_CALLBACK_URL) console.log('config FACEBOOK_CALLBACK_URL missing');
+if (!process.env.FACEBOOK_GROUP) console.log('config FACEBOOK_GROUP missing');
 
 module.exports = {
-  EMAIL_API_KEY: process.env.EMAIL_API_KEY ? process.env.EMAIL_API_KEY.trim() : '' || 'mailgun api key here',
-  EMAIL_DOMAIN: process.env.EMAIL_DOMAIN ? process.env.EMAIL_DOMAIN.trim() : '' || 'mailgun api key here',
-  MAP_BOX_USER: process.env.MAP_BOX_USER ? process.env.MAP_BOX_USER.trim() : '' || 'mailgun api key here',
-  MAP_BOX_API_KEY: process.env.MAP_BOX_API_KEY? process.env.MAP_BOX_API_KEY.trim() : '' || 'mailgun api key here',
-  MAP_BOX_MAP_ID: process.env.MAP_BOX_MAP_ID ? process.env.MAP_BOX_MAP_ID.trim() : '' || 'mailgun api key here',
-  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY ? process.env.GOOGLE_API_KEY.trim() : '' || 'mailgun api key here',
-  GOOGLE_CALENDAR_ID: process.env.GOOGLE_CALENDAR_ID ? process.env.GOOGLE_CALENDAR_ID.trim() : '' || 'mailgun api key here', 
-  FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID ? process.env.FACEBOOK_APP_ID.trim() : '' || 'mailgun api key here',
-  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET ? process.env.FACEBOOK_SECRET.trim() : '' || 'mailgun api key here',
-  FACEBOOK_NAMESPACE: process.env.FACEBOOK_NAMESPACE ? process.env.FACEBOOK_NAMESPACE.trim() : '' || 'mailgun api key here',
-  FACEBOOK_CALLBACK_URL: process.env.FACEBOOK_CALLBACK_URL ? process.env.FACEBOOK_CALLBACK_URL.trim() : '' || 'mailgun api key here',
-  FACEBOOK_GROUP: process.env.FACEBOOK_GROUP ? process.env.FACEBOOK_GROUP.trim() : '' || 'mailgun api key here' 
+  EMAIL_API_KEY: process.env.EMAIL_API_KEY ? process.env.EMAIL_API_KEY.trim() : '',
+  EMAIL_DOMAIN: process.env.EMAIL_DOMAIN ? process.env.EMAIL_DOMAIN.trim() : '',
+  MAP_BOX_USER: process.env.MAP_BOX_USER ? process.env.MAP_BOX_USER.trim() : '',
+  MAP_BOX_API_KEY: process.env.MAP_BOX_API_KEY? process.env.MAP_BOX_API_KEY.trim() : '',
+  MAP_BOX_MAP_ID: process.env.MAP_BOX_MAP_ID ? process.env.MAP_BOX_MAP_ID.trim() : '',
+  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY ? process.env.GOOGLE_API_KEY.trim() : '',
+  GOOGLE_CALENDAR_ID: process.env.GOOGLE_CALENDAR_ID ? process.env.GOOGLE_CALENDAR_ID.trim() : '', 
+  FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID ? process.env.FACEBOOK_APP_ID.trim() : '',
+  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET ? process.env.FACEBOOK_SECRET.trim() : '',
+  FACEBOOK_NAMESPACE: process.env.FACEBOOK_NAMESPACE ? process.env.FACEBOOK_NAMESPACE.trim() : '',
+  FACEBOOK_CALLBACK_URL: process.env.FACEBOOK_CALLBACK_URL ? process.env.FACEBOOK_CALLBACK_URL.trim() : '',
+  FACEBOOK_GROUP: process.env.FACEBOOK_GROUP ? process.env.FACEBOOK_GROUP.trim() : ''
 };
--- server/config/serverSettings.js
+++ server/config/serverSettings.js
@@ -1,12 +1,19 @@
-var databaseName = "notinphilly";
+if (!process.env.ADMIN_EMAIL) console.log('config ADMIN_EMAIL missing');
+if (!process.env.WEB_SITE_URL) console.log('config WEB_SITE_URL missing');
+if (!process.env.DATABASE_NAME) console.log('config DATABASE_NAME missing');
+if (!process.env.AUTH_COOKIE_NAME) console.log('config AUTH_COOKIE_NAME missing');
+if (!process.env.SECRET_TOKEN) console.log('config SECRET_TOKEN missing');
+if (!process.env.HTTP_IP) console.log('config HTTP_IP missing');
+if (!process.env.HTTP_PORT) console.log('config HTTP_PORT missing');
+if (!process.env.DB_CONNECTION_STRING) console.log('config DB_CONNECTION_STRING missing');
 
 module.exports = {
-  ADMIN_EMAIL: process.env.ADMIN_EMAIL ? process.env.ADMIN_EMAIL.trim() : '' || 'mailgun api key here',
-  WEB_SITE_URL: process.env.WEB_SITE_URL? process.env.WEB_SITE_URL.trim() : '' || 'mailgun api key here',
-  DATABASE_NAME: process.env.DATABASE_NAME ? process.env.DATABASE_NAME.trim() : '' || 'mailgun api key here',
-  AUTH_COOKIE_NAME: process.env.AUTH_COOKIE_NAME? process.env.AUTH_COOKIE_NAME.trim() : '' || 'mailgun api key here',
-  SECRET_TOKEN: process.env.SECRET_TOKEN? process.env.SECRET_TOKEN.trim() : '' || 'mailgun api key here',
-  HTTP_IP: process.env.HTTP_IP ? process.env.HTTP_IP.trim() : '' || 'mailgun api key here',
-  HTTP_PORT: process.env.HTTP_PORT ? process.env.HTTP_PORT.trim() : '' || 'mailgun api key here',
-  DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING ? process.env.DB_CONNECTION_STRING.trim() : '' || 'mailgun api key here',
+  ADMIN_EMAIL: process.env.ADMIN_EMAIL ? process.env.ADMIN_EMAIL.trim() : '',
+  WEB_SITE_URL: process.env.WEB_SITE_URL? process.env.WEB_SITE_URL.trim() : '',
+  DATABASE_NAME: process.env.DATABASE_NAME ? process.env.DATABASE_NAME.trim() : '',
+  AUTH_COOKIE_NAME: process.env.AUTH_COOKIE_NAME? process.env.AUTH_COOKIE_NAME.trim() : '',
+  SECRET_TOKEN: process.env.SECRET_TOKEN? process.env.SECRET_TOKEN.trim() : '',
+  HTTP_IP: process.env.HTTP_IP ? process.env.HTTP_IP.trim() : '',
+  HTTP_PORT: process.env.HTTP_PORT ? process.env.HTTP_PORT.trim() : '',
+  DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING ? process.env.DB_CONNECTION_STRING.trim() : ''
 };
